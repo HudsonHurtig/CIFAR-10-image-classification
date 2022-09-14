facbce7b0eb2a4a5657f7d81387e982160da9e3a
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-14 01:02:55 -0500

    added some more model stuff, ran into cuda core error, will fix tom

--- classification-poject-1.py
+++ classification-poject-1.py
@@ -43,9 +43,44 @@ class_num = y_test.shape[1]
 
 # creating layers of a nueral net
 model = keras.Sequential()
+
+#adding the first layer
+#padding ensures that all values are of same input length
+#there are 64 filters to this layer and there are 3 dimension to each of these filters or "averages"
+#the relu activation is more of a all or none type of summation
+
 model.add(keras.layers.Conv2D(64, 3, activation='relu', padding='same'))
+#max pooling maximizes the value that each feature can take on
 model.add(keras.layers.MaxPooling2D(2))
+#this destroys connections to prevent overfitting
+model.add(keras.layers.Dropout(0.2))
+#ensures that new nuerons arent created with too much to it 
+model.add(keras.layers.BatchNormalization())
+    
+
+#same thing as above except it has more filters(sugested to go by factors of 2), doesnt need max pooling because its taking data thats already been normalized?
+model.add(keras.layers.Conv2D(128, 3, activation='relu', padding='same'))
 model.add(keras.layers.Dropout(0.2))
 model.add(keras.layers.BatchNormalization())
+
+#preparation for output
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dropout(0.2))
+
+# find difference between flatten and dense?
+
+#preparation for classification?
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dropout(0.3))
+model.add(keras.layers.BatchNormalization())
+
+
+#output layer
+model.add(keras.layers.Dense(class_num, activation='softmax'))
+
+#run network
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'val_accuracy'])
+
+
 
 
